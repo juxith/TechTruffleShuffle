0cc6d7073d1b0054c9095abd53383537be3a5e73
Alexander Rohde <alrohde10@gmail.com>
2017-10-27 16:04:37 -0500

    Aj's identity commit 10-27-2017

--- TechTruffleShuffle/TechTruffleShuffle.Data/Migrations/201710271925502_reinit.Designer.cs
+++ TechTruffleShuffle/TechTruffleShuffle.Data/Migrations/201710271925502_reinit.Designer.cs
@@ -7,13 +7,13 @@ namespace TechTruffleShuffle.Data.Migrations
     using System.Resources;
     
     [GeneratedCode("EntityFramework.Migrations", "6.1.3-40302")]
-    public sealed partial class init : IMigrationMetadata
+    public sealed partial class reinit : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(init));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(reinit));
         
         string IMigrationMetadata.Id
         {
-            get { return "201710261946052_init"; }
+            get { return "201710271925502_reinit"; }
         }
         
         string IMigrationMetadata.Source
--- TechTruffleShuffle/TechTruffleShuffle.Data/Migrations/Configuration.cs
+++ TechTruffleShuffle/TechTruffleShuffle.Data/Migrations/Configuration.cs
@@ -1,5 +1,7 @@
 namespace TechTruffleShuffle.Data.Migrations
 {
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
@@ -14,18 +16,53 @@ namespace TechTruffleShuffle.Data.Migrations
 
         protected override void Seed(TechTruffleShuffle.Data.TechTruffleShuffleEntities context)
         {
-            //  This method will be called after migrating to the latest version.
+            // Load the user and role managers with our custom models
+            var userMgr = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+            var roleMgr = new RoleManager<ApplicationRole>(new RoleStore<ApplicationRole>(context));
 
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data. E.g.
-            //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
-            //
+
+            // have we loaded roles already?
+            if (!roleMgr.RoleExists("admin"))
+            {
+                roleMgr.Create(new ApplicationRole() { Name = "admin" });
+            }
+
+            if (!roleMgr.RoleExists("author"))
+            {
+                roleMgr.Create(new ApplicationRole() { Name = "author" });  
+            }
+
+             var authorUser = new ApplicationUser()
+             {
+                 UserName = "authorUser"
+             };
+
+             var user = new ApplicationUser()
+             {
+                 UserName = "adminUser"
+             };
+
+            if(!userMgr.Users.Any(u => u.UserName == "authorUser"))
+            {
+                userMgr.Create(authorUser, "testing123");
+
+                context.SaveChanges();
+
+                userMgr.AddToRole(authorUser.Id, "author");
+            }
+
+            if (!userMgr.Users.Any(u => u.UserName == "adminUser"))
+            {
+                userMgr.Create(user, "testing123");
+
+                context.SaveChanges();
+
+                userMgr.AddToRole(user.Id, "admin");
+            }       
+
+            context.SaveChanges();
+            return;
+
         }
     }
 }
