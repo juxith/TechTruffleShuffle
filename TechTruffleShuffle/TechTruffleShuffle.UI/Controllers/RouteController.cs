0156618fa5bc8f57c5a8459af3254406d9ebdfff
Lindsey Parlow <lmptipper@hotmail.com>
2017-10-31 13:11:35 -0500

    start of routes

---
+++ TechTruffleShuffle/TechTruffleShuffle.UI/Controllers/RouteController.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using TechTruffleShuffle.Data;
+using TechTruffleShuffle.Models;
+
+namespace TechTruffleShuffle.UI.Controllers
+{
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    public class RouteController : ApiController
+    {
+        [Route("blogs")]
+        [AcceptVerbs("GET")]
+        public IHttpActionResult GetAllPosts()
+        {
+            List<BlogPost> blogPosts = TechTruffleRepositoryFactory.Create().GetAllPosts();
+
+            if (blogPosts == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Ok(blogPosts);
+            }
+        }
+
+        [Route("blogs/isRemoved/{true}")]
+        [AcceptVerbs("GET")]
+        public IHttpActionResult GetAllRemovedPosts()
+        {
+            List<BlogPost> blogPosts = TechTruffleRepositoryFactory.Create().GetAllRemovedPosts();
+
+            if (blogPosts == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Ok(blogPosts);
+            }
+        }
+
+        //[Route("blog/{blogId}")]
+        //[AcceptVerbs("GET")]
+        //public IHttpActionResult GetAllPosts()
+        //{
+
+        //}
+
+        //[Route("blog/{blogId}")]
+        //[AcceptVerbs("GET")]
+        //public IHttpActionResult GetAllPosts()
+        //{
+
+        //}
+
+        //[Route("blog/{blogId}")]
+        //[AcceptVerbs("GET")]
+        //public IHttpActionResult GetAllPosts()
+        //{
+
+        //}
+
+        //[Route("blog/{blogId}")]
+        //[AcceptVerbs("GET")]
+        //public IHttpActionResult GetAllPosts()
+        //{
+
+        //}
+
+        //[Route("blog/{blogId}")]
+        //[AcceptVerbs("GET")]
+        //public IHttpActionResult GetAllPosts()
+        //{
+
+        //}
+
+        //[Route("blog/{blogId}")]
+        //[AcceptVerbs("GET")]
+        //public IHttpActionResult GetAllPosts()
+        //{
+
+        //}
+
+        //[Route("blog/{blogId}")]
+        //[AcceptVerbs("GET")]
+        //public IHttpActionResult GetAllPosts()
+        //{
+
+        //}
+
+        //[Route("blog/{blogId}")]
+        //[AcceptVerbs("GET")]
+        //public IHttpActionResult GetAllPosts()
+        //{
+
+        //}
+
+        //[Route("blog/{blogId}")]
+        //[AcceptVerbs("GET")]
+        //public IHttpActionResult GetAllPosts()
+        //{
+
+        //}
+
+        //[Route("blog/{blogId}")]
+        //[AcceptVerbs("GET")]
+        //public IHttpActionResult GetAllPosts()
+        //{
+
+        //}
+
+        //[Route("blog/{blogId}")]
+        //[AcceptVerbs("GET")]
+        //public IHttpActionResult GetAllPosts()
+        //{
+
+        //}
+
+        //[Route("blog/{blogId}")]
+        //[AcceptVerbs("GET")]
+        //public IHttpActionResult GetAllPosts()
+        //{
+
+        //}
+
+        //[Route("blog/{blogId}")]
+        //[AcceptVerbs("GET")]
+        //public IHttpActionResult GetAllPosts()
+        //{
+
+        //}
+
+        //[Route("blog/{blogId}")]
+        //[AcceptVerbs("GET")]
+        //public IHttpActionResult GetAllPosts()
+        //{
+
+        //}
+
+        //[Route("blog/{blogId}")]
+        //[AcceptVerbs("GET")]
+        //public IHttpActionResult GetAllPosts()
+        //{
+
+        //}
+
+        //[Route("blog/{blogId}")]
+        //[AcceptVerbs("GET")]
+        //public IHttpActionResult GetAllPosts()
+        //{
+
+        //}
+    }
+}
---
+++ TechTruffleShuffle/TechTruffleShuffle.UI/Scripts/home.js
@@ -0,0 +1,11 @@
+﻿$(document).ready(function () {
+
+});
+
+//function loadBlogs() {
+//	$.ajax({
+//		type: "GET",
+//		url: "http://localhost:6245/items",
+//		success: function (itemArray) {
+//			var blogPosts = $("#
+//}
